56358ae4d515d08c42cac13f5f08319bc5336e7e
moh. nurwahid <mohnurwaid923@gmail.com>
2024-12-17 17:37:05 +0700

    memperbaiki LocalRepositoryMhs

--- app/src/main/java/com/example/project9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/project9/repository/LocalRepositoryMhs.kt
@@ -1,12 +1,30 @@
 package com.example.project9.repository
 
+
 import com.example.project9.Data.dao.MahasiswaDao
 import com.example.project9.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs{
-    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
