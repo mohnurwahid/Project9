cce2c37ca5b20575df016df05303e337edf08753
moh. nurwahid <mohnurwaid923@gmail.com>
2024-12-17 17:34:50 +0700

    mengupdate RepositoryMhs

--- app/src/main/java/com/example/project9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/project9/repository/RepositoryMhs.kt
@@ -1,7 +1,17 @@
 package com.example.project9.repository
 
 import com.example.project9.Data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
+
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs () : Flow<List<Mahasiswa>>
+
+    fun  getMhs (nim: String) : Flow<Mahasiswa>
+
+    suspend fun deleteMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs (mahasiswa: Mahasiswa)
 }
