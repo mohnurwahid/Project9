ca788c96e34e54894e9719a640bdecc091fd7fb4
moh. nurwahid <mohnurwaid923@gmail.com>
2024-12-17 17:35:11 +0700

    mengupdate PenyediaViewModel

--- app/src/main/java/com/example/project9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project9/ui/viewmodel/PenyediaViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.project9.KrsApp
 
 object PenyediaViewModel{
 
